ed7ca959903e616c013a09b6b6b862aac1f6d57d
Mitch Wenman <mitchwenman@gmail.com>
2016-12-31 09:08:51 +1000

    still not working

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os
 import sys
 import json
+import logging
 from AppSettings import Settings
 
+## Fix for requests
 import requests
+import requests_toolbelt.adapters.appengine
+requests_toolbelt.adapters.appengine.monkeypatch()
+
 from flask import Flask, request
+from pymessenger.bot import Bot
+
+
+
 
 app = Flask(__name__)
 
@@ -21,7 +30,6 @@ def verify():
         if not request.args.get("hub.verify_token") == Settings.get("VERIFY_TOKEN"):
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-
     return "Hello world", 200
 
 
@@ -42,9 +50,9 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    #message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_login_button(sender_id)
+                    send_message(sender_id, "Received")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -83,26 +91,11 @@ def send_login_button(recipient_id):
 def send_message(recipient_id, message_text):
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-    params = {
-        "access_token": Settings.get("PAGE_ACCESS_TOKEN")
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
+    bot = Bot(Settings.get("PAGE_ACCESS_TOKEN"))
+    bot.send_message(recipient_id, message_text)
+    log("Message sent")
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
-    sys.stdout.flush()
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.info(message)
